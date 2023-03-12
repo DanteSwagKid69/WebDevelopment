14f6efd52d42b20e861e6d8c255963d601d19609
Dante von Matern <112094212+DanteSwagKid69@users.noreply.github.com>
2023-03-12 13:21:08 +0100

    Delete PROJEKT2 directory

--- PROJEKT2/js/preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", (e) => {
-    const preloader = document.querySelector(".preloader");
-    setInterval(function () {
-        preloader.style.display = "none";  
-    }, 1000);
-});
